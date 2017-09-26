e77f173f385972e28d9fc92141aec7995a728449
HazzaCheng <hazzacheng@gmail.com>
2017-09-26 22:10:08 +0800

    Initial commit

---
+++ src/main/scala/com/hazzacheng/FD/Main.scala
@@ -0,0 +1,14 @@
+package com.hazzacheng.FD
+
+/**
+  * Created with IntelliJ IDEA.
+  *
+  * Description: 
+  * User: HazzaCheng
+  * Contact: hazzacheng@gmail.com
+  * Date: 2017-09-26
+  * Time: 9:44 PM
+  */
+object Main {
+
+}
