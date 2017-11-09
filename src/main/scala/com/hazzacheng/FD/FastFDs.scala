b1364dd58671bb8af24a68995388e6f43d9dae07
HazzaCheng <hazzacheng@gmail.com>
2017-11-09 20:10:31 +0800

    get ag

--- src/main/scala/com/hazzacheng/FD/FastFDs.scala
+++ src/main/scala/com/hazzacheng/FD/FastFDs.scala
@@ -3,6 +3,7 @@ package com.hazzacheng.FD
 import org.apache.spark.SparkContext
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
 
 import scala.collection.mutable
 
@@ -18,43 +19,56 @@ import scala.collection.mutable
 object FastFDs {
   var time1 = 0L
 
-  def genDiffSets(sc: SparkContext, rdd: RDD[(Array[String], Long)],
-                  colSize: Int): Array[Set[Int]] = {
+  def genDiffSets(sc: SparkContext,
+                  rdd: RDD[(Array[String], Long)],
+                  colSize: Int): Set[Set[Int]] = {
     genAgreeSets(sc, rdd, colSize)
   }
 
-  private def genAgreeSets(sc: SparkContext, rdd: RDD[(Array[String], Long)],
-                  colSize: Int): Array[Set[Int]] = {
+  private def genAgreeSets(sc: SparkContext,
+                           rdd: RDD[(Array[String], Long)],
+                           colSize: Int): Set[Set[Int]] = {
     val sets = mutable.HashSet.empty[Set[Int]]
-    val striped = new Array[Array[(Set[Int], Int)]](colSize)
+    val stripped = new Array[Array[Set[Int]]](colSize)
     for (i <- 1 to colSize) {
       val temp = getStripPartitions(sc, rdd, i).collect()
-      striped(i - 1) = temp.zipWithIndex
+      stripped(i - 1) = temp
       sets ++= temp
     }
     val mc = getMC(sc, sets)
+    time1 = System.currentTimeMillis()
+    val rows = getRows(mc)
+    println("====USE TIME get rows: " + (System.currentTimeMillis() - time1))
+    val ecMap = getAllEc(sc, stripped, rows)
     val couplesRdd = getAllCouples(sc, mc)
+    val ag = getAg(sc, couplesRdd, ecMap)
+    println("====Size ag sets: " + ag.size)
+
+    ag
   }
 
-  private def getStripPartitions(sc: SparkContext, rdd: RDD[(Array[String], Long)],
-             attribute: Int): RDD[Set[Int]] = {
+  private def getStripPartitions(sc: SparkContext,
+                                 rdd: RDD[(Array[String], Long)],
+                                 attribute: Int): RDD[Set[Int]] = {
     val partitions = rdd.map(line => (line._1(attribute - 1), List(line._2.toInt)))
       .reduceByKey(_ ++ _).map(_._2.toSet)
 
     partitions
   }
 
-  private def getMC(sc: SparkContext, sets: mutable.HashSet[Set[Int]]): Array[Set[Int]] = {
+  private def getMC(sc: SparkContext,
+                    sets: mutable.HashSet[Set[Int]]): Array[Set[Int]] = {
     time1 = System.currentTimeMillis()
     val temp = sets.toArray.map(x => (x.size, x)).sortWith((x, y) => x._1 > y._1)
-    println("===========USE TIME sort: " + (System.currentTimeMillis() - time1))
+    println("====USE TIME sort: " + (System.currentTimeMillis() - time1))
     val tempBV = sc.broadcast(temp)
     val mc = sc.parallelize(temp).filter(isBigSet(tempBV, _)).collect()
-
+    println("====Size mc" + mc.length)
     mc.map(x => x._2)
   }
 
-  private def isBigSet(tempBV: Broadcast[Array[(Int,Set[Int])]], s: (Int,Set[Int])): Boolean = {
+  private def isBigSet(tempBV: Broadcast[Array[(Int,Set[Int])]],
+                       s: (Int,Set[Int])): Boolean = {
     val arr = tempBV.value
     for (set <- arr) {
       if(s._1 <= set._1) {
@@ -65,8 +79,9 @@ object FastFDs {
   }
 
   private def getAllCouples(sc: SparkContext,
-                            mc: Array[Set[Int]]): RDD[scala.List[(Int, Int)]] = {
-    val couples = sc.parallelize(mc).map(set => getCouples(set)).distinct()
+                            mc: Array[Set[Int]]): RDD[(Int, Int)] = {
+    val couples = sc.parallelize(mc).flatMap(set => getCouples(set))
+      .distinct().persist(StorageLevel.MEMORY_AND_DISK_SER)
 
     couples
   }
@@ -80,6 +95,65 @@ object FastFDs {
         list.append((arr(i), arr(j)))
 
     list.toList
+  }
+
+  private def getRows(mc: Array[Set[Int]]): Array[Int] = {
+    val rows = mc.flatMap(_.toList)
+
+    rows.distinct
+  }
+
+  private def getAllEc(sc: SparkContext,
+                       stripped: Array[Array[Set[Int]]],
+                       rows: Array[Int]): mutable.HashMap[Int, Set[(Int, Int)]] = {
+    val strippedBV = sc.broadcast(stripped)
+    val ec = sc.parallelize(rows).map(r => getEc(strippedBV, r)).collect()
+    val ecMap = mutable.HashMap.empty[Int, Set[(Int, Int)]]
+    time1 = System.currentTimeMillis()
+    ec.foreach(x => ecMap.put(x._1, x._2))
+    println("====USE TIME get ec: " + (System.currentTimeMillis() - time1))
+
+    ecMap
+  }
+
+  private def getEc(strippedBV: Broadcast[Array[Array[Set[Int]]]],
+                    r: Int): (Int, Set[(Int, Int)]) = {
+    val stripped = strippedBV.value
+    val res = mutable.ListBuffer.empty[(Int, Int)]
+    val len1 = stripped.length
+    for (i <- 0 until len1) {
+      val len2 = stripped(i).length
+      var nonFind = true
+      var j = 0
+      while (nonFind && j < len2) {
+        if (stripped(i)(j) contains r) {
+          res.append((i + 1, j))
+          j += 1
+          nonFind = false
+        }
+      }
+    }
+
+    (r, res.toSet)
+  }
+
+  private def getAg(sc: SparkContext,
+                    couplesRdd: RDD[(Int, Int)],
+                    ecMap: mutable.HashMap[Int, Set[(Int, Int)]]): Set[Set[Int]] = {
+    val ecMapBV = sc.broadcast(ecMap)
+    val ag = couplesRdd.map(couple => getCommon(ecMapBV, couple._1, couple._2)).collect()
+
+    ag.toSet
+  }
+
+  private def getCommon(ecMapBV: Broadcast[mutable.HashMap[Int, Set[(Int, Int)]]],
+                        i: Int, j: Int): Set[Int] = {
+    val ecMap = ecMapBV.value
+    val s1 = ecMap(i)
+    val s2 = ecMap(j)
+    val common = (s1 & s2).map(_._1)
+
+    common
   }
 
 /*  def getAgreeSets(p: List[Array[String]], commonAttr: Int, colSize: Int) = {
