70690302a65f5c893909a8ab623648bcd27a63d6
HazzaCheng <hazzacheng@gmail.com>
2017-11-15 18:28:00 +0800

    fix bug: cut leaves

--- src/main/scala/com/hazzacheng/FD/FDsMine.scala
+++ src/main/scala/com/hazzacheng/FD/FDsMine.scala
@@ -1,13 +1,11 @@
 package com.hazzacheng.FD
 
-import com.hazzacheng.FD.FDUtils.{takeAttrLHS, takeAttrRHS}
 import org.apache.spark.SparkContext
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 
 /**
   * Created with IntelliJ IDEA.
@@ -27,15 +25,15 @@ object FDsMine {
   def findOnSpark(sc: SparkContext, rdd: RDD[Array[String]],
                   colSize: Int, orders: Array[(Int, Long)]): Map[Set[Int], List[Int]] = {
     val dependencies = FDUtils.getDependencies(colSize)
-    val emptyFD = mutable.Set.empty[Int]
-    val results = mutable.ListBuffer.empty[(Set[Int], Int)]
+    val emptyFD = mutable.HashSet.empty[Int]
+    val results = mutable.HashSet.empty[(Set[Int], Int)]
 
     for (i <- orders) {
       val partitionSize = i._2.toInt
       if (partitionSize == 1) emptyFD.add(i._1)
       time2 = System.currentTimeMillis()
       val candidates = FDUtils.getCandidateDependencies(dependencies, i._1)
-      val lhsWithSize = candidates.toList.groupBy(_._1.size)
+      //val lhsWithSize = candidates.toList.groupBy(_._1.size)
       //val keys = lhsWithSize.keys.toList.sortWith((x, y) => x < y)
 
       val partitionsRDD = repart(sc, rdd, i._1).persist(StorageLevel.MEMORY_AND_DISK_SER)
@@ -54,7 +52,7 @@ object FDsMine {
           time1 = System.currentTimeMillis()
           println("====Size successed:"  + i + " " + k + " " + minimalFDs.size)
           if (minimalFDs.nonEmpty) {
-            cutLeaves(candidates, minimalFDs, i._1)
+            cutLeaves(candidates, results, minimalFDs)
             println("====Cut Leaves in " + k + " Use Time=============" + (System.currentTimeMillis() - time1))
           }
         }
@@ -63,7 +61,7 @@ object FDsMine {
       println("====Common Attr in " + i + " Use Time=============" + (System.currentTimeMillis() - time2))
     }
     if (emptyFD.nonEmpty)
-      emptyFD.foreach(rhs => results.append((Set.empty[Int], rhs)))
+      emptyFD.foreach(rhs => results.add((Set.empty[Int], rhs)))
 
     results.toList.groupBy(_._1).map(x => (x._1, x._2.map(_._2)))
   }
@@ -80,7 +78,7 @@ object FDsMine {
   def checkPartitionsRDD(sc: SparkContext,
                       partitionsRDD: RDD[List[Array[String]]],
                       fds: List[(Set[Int], mutable.Set[Int])],
-                      res: mutable.ListBuffer[(Set[Int], Int)],
+                      res: mutable.HashSet[(Set[Int], Int)],
                       partitionSize: Int,
                       colSize: Int): Array[(Set[Int], Int)] = {
     val fdsBV = sc.broadcast(fds)
@@ -150,15 +148,19 @@ object FDsMine {
   }
 
   def cutLeaves(candidates: mutable.HashMap[Set[Int], mutable.Set[Int]],
-                successed: Array[(Set[Int], Int)], commonAttr: Int): Unit = {
+                results: mutable.HashSet[(Set[Int], Int)],
+                successed: Array[(Set[Int], Int)]): Unit = {
     for (d <- successed) {
       val lend = d._1.size
-      val lhs = candidates.keys.filter(x => (x.size > lend && (x & d._1) == d._1)).toList
-      cut(candidates, lhs, d._2)
+      val lhs = candidates.keys.filter(x => x.size > lend && isSubSet(x, d._1)).toList
+      val nonMinimal = results.toList.filter(x =>
+        x._1.size > lend && isSubSet(x._1, d._1) && x._2 == d._2)
+      cutInCandidates(candidates, lhs, d._2)
+      nonMinimal.foreach(fd => results.remove(fd))
     }
   }
 
-  def cut(candidates: mutable.HashMap[Set[Int], mutable.Set[Int]],
+  def cutInCandidates(candidates: mutable.HashMap[Set[Int], mutable.Set[Int]],
           lhs: List[Set[Int]],
           rhs: Int): Unit = {
     for (l <- lhs) {
@@ -171,7 +173,12 @@ object FDsMine {
         }
       }
     }
+  }
 
+  def isSubSet(big: Set[Int], small: Set[Int]): Boolean = {
+    small.toList.foreach(s => if (!big.contains(s)) return false)
+
+    true
   }
 
   def takeAttrLHS(arr: Array[String],
